8b1b077b906e05b3acefa4cfab841988ab09e681
Abdullah Al Jubair <jubair2810@gmail.com>
2026-02-06 20:59:40 +0600

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className=" h-screen flex justify-center items-center">
+      <Button> Click Me </Button>
+    </div>
+  );
+};
+
+export default HomePage;
